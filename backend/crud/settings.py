98d954454e14611bbe6c86142574fab0587738f3
Zhino-ui <zhino4u2010@gmail.com>
2024-11-29 17:46:53 +0000

    procfile created

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # This is the static directory in your project folder
-    BASE_DIR / "frontend/dist",  # Path to your Vite build directory
+    BASE_DIR / "frontend" / "dist",  # Path to your Vite build directory
 ]
 
 STATIC_URL = "/static/"
